a06da3fa1dd641802745bd70bb029642534dbb44
DodoT <dorian_thivolle@orange.fr>
2018-02-09 07:19:41 +0100

    Add docstring on simple classes

---
+++ websrv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" Web Service Command """
+
+class SyncWebService:
+    """ SyncWebSevice is an Syncronous web service.
+
+    It provide simple http request and wait the response.
+    """
+    pass
+
+class AsyncWebService:
+    """ AsyncWebSevice is an asyncronous web service.
+
+    It provide websocket technologie to have a two-way exange
+    with the connected clients.
+
+    TODO: Make some test with the web sockets
+    """
+    pass
